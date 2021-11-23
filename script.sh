1e2819e86d99309400cdff60f15b30de5b445221
Zero <john.royer@gmail.com>
2021-11-23 13:27:43 +0800

    Fetch config from Github

--- script.sh
+++ script.sh
@@ -19,13 +19,13 @@ apt-get update
 
 
 # default config
-wget https://lab.zeroplex.tw/linode/config.zip -O /root/config.zip
+wget https://raw.githubusercontent.com/johnroyer/linode-stackscript-ubuntu-20.04-LNMP/master/config/nginx-default -O /root/nginx-default
+wget https://raw.githubusercontent.com/johnroyer/linode-stackscript-ubuntu-20.04-LNMP/github-hosted/config/www.conf -O /root/www.conf
 cd /root
-unzip config.zip
 
 
 # Nginx
-cp config/nginx/sites-available/default /etc/nginx/sites-available/default
+cp /root/nginx-default /etc/nginx/sites-available/default
 systemctl restart nginx.service
 
 # PHP
@@ -35,6 +35,6 @@ apt-get install -y \
     php8.0-imagick php8.0-intl php8.0-mbstring php8.0-mcrypt php8.0-memcached \
     php8.0-mysql php8.0-readline php8.0-redis php8.0-sqlite3 \
     php8.0-xml php8.0-xsl php8.0-zip 
-cp config/php/8.0/fpm/pool.d/www.conf /etc/php/8.0/fpm/pool.d/
+cp /root/www.conf /etc/php/8.0/fpm/pool.d/
 systemctl restart php8.0-fpm.service
 
